26c91376bd813b99b5924255e32807bfc3265774
o-kauan <o.kkauan.lima21@gmail.com>
2026-03-20 11:31:39 -0300

    Aula 2 dia 03/20/26: variáveis e operadores

---
+++ aula02-variaveis-operadores/Atividade.py
@@ -0,0 +1,17 @@
+# DESAFIO 1
+nome = input("Qual é o seu nome: ")
+k = int(input("Selecione a mensagem de boas vindas (1, 2 ou 3): "))
+
+if k == 1:
+    print(f"Bom dia {nome}")
+if k == 2:
+    print(f"Boa tarde {nome}")
+if k == 3:
+    print(f"Boa noite {nome}")
+
+# DESAFIO 2
+dia = 21
+ano = 2007
+mes = 11
+
+print(f"{dia}/{mes}/{ano}")
---
+++ aula02-variaveis-operadores/aula02a.py
@@ -0,0 +1,32 @@
+print ("Olá mundo")
+
+print(7+4)
+print("7+4")
+print("7"+"4") # CONCATENAÇÃO DE STRINGS
+
+# Comentário de 1 linha
+
+'''
+COMENTÁRIOS DE
+MÚLTIPLAS
+LINHAS
+'''
+
+# VARIÁVEIS
+
+nome = "Kauan" # str
+idade = 18 # int
+peso = 70.2 # float
+
+print(nome,idade,peso)
+print("nome:", nome,"idade:", idade,"peso:",peso)
+print(f"Querido {nome}, cujos peso e idade equivalem, respectivamente a {peso} e {idade}, como vai você?")
+
+# INPUT - SIMULAÇÃO DE UM FORMS NO CMD
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+nova_idade = idade + 10
+
+print(nome, idade)
+print(idade + 1)
+print(nova_idade)
