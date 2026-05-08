3e823abada213d3b1103c29cec2bfbe48903d7f3
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-08 11:22:36 -0300

    add matriz

---
+++ aula05 - arrays/matrizes.py
@@ -0,0 +1,13 @@
+musicas = [
+    ['Chigago', 'Michael Jackson'],
+    ['Sorry', 'Justin Bieber'],
+    ['Judas', 'Lady Gaga']
+]
+print(musicas[1][0])
+
+for musica in musicas:
+    for info in musica:
+        print(info)
+
+for musica in musicas:
+    print(musica[0])
