13a8a3f4b73e89642e297500fd3e1f10c4785a0a
o-kauan <rm573727@fiap.com.br>
2026-09-18 11:33:03 -0300

    Add novas funções para exportar para csv e realizar query, o método numerate basicamente cria um dicionário com a key e o valor

--- aula08 - modulamento/app.py
+++ aula08 - modulamento/app.py
@@ -9,23 +9,50 @@ def add_lead ():
 # 1 validar dados
 # 1,5 estruturar em dicionários
 # 2 adicionar na base de dados
-
-    print(model_lead(name, email, stage))
-
+# 2.5 print(model_lead(name, email, stage))
 # 3 enviar os dados para o leads.json
 # 4 o control.py irá auxiliar a enviar os dados para o
 
     control.create_lead(model_lead(name, email, stage))
 
 def list_leads():
-    leads = control.read_lead()
-    print(leads)
+    leads = control.read_leads()
+    print(f"ID | {"Nome":<10} | {"E-mail"}")
+    print("-" * 50)
+    for i, lead in enumerate(leads):
+        print(f"{i:02d} | {lead["name"]:<10} | {lead["email"]}")
+
+def search_leads():
+    query = input("Buscar por: ").strip().lower()
+    if not query:
+        print("Consulta Vazia :c")
+        return
+
+    # chamar o control e passar nossa query (busca)
+    # o control irá verificar se existe a query no leads.json
+    # e irá retornar os resultados da busca
+    found_leads = control.read_leads_search(query)
+
+    print(f"ID | {"Nome":<10} | {"E-mail"}")
+    print("-" * 50)
+    for i, lead in found_leads:
+        print(f"{i:02d} | {lead["name"]:<10} | {lead["email"]}")
+
+def export_leads():
+    path_csv = control.export_csv()
+
+    if path_csv is None:
+        print("Não foi possível exportar os leads")
+    else:
+        print(f"Leads {path_csv}")
 
 def main ():
     while True:
         print("\nMini CRM de Leads")
         print("[1] adicionar lead")
         print("[2] lista de leads")
+        print("[3] Buscar (nome/email)")
+        print("[4] Exportar para CSV")
         print("[0] Sair do sistema")
 
         opt = input("Escolha uma opcção: ")
@@ -36,6 +63,14 @@ def main ():
         elif opt == "2":
             print("Lista de Leads")
             list_leads()
+
+        elif opt == "3":
+            print("Buscar (nome ou email)")
+            search_leads()
+
+        elif opt == "4":
+            print("Expotando para CSV...")
+            export_leads()
 
         elif opt == "0":
             print("Saindo...")
--- aula08 - modulamento/control.py
+++ aula08 - modulamento/control.py
@@ -1,6 +1,5 @@
-from idlelib.iomenu import encoding
-from pathlib import Path #manipular pastas
-import json #dicionário != joson
+from pathlib import Path # manipular pastas
+import json, csv # dicionário != json
 
 DATA_DIR = Path(__file__).resolve().parent / "data"
 # print(DATA_DIR) # = C:\Users\labsfiap\PycharmProjects\1CCA-Python-2026\aula08 - modulamento\data\leads.json
@@ -11,8 +10,7 @@ DB_PATH = DATA_DIR / "leads.json"
 # CREATE / READ / UPDATE / DELETE
 
 # --- READ ---
-
-def read_lead():
+def read_leads():
     if not DB_PATH.exists():
         return [] # 0 leads
     try:
@@ -20,11 +18,37 @@ def read_lead():
     except json.JSONDecodeError:                       # utf-8 é o padrão de codificação nosso portugueso e acentos e ç
         return [] #  bruh
 
-print(read_lead())
+print(read_leads())
 
 # --- CREATE ---
-
 def create_lead (lead_dict):
-    leads = read_lead()
+    leads = read_leads() #não pode carregar a base toda toda hora
     leads.append(lead_dict)
-    DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, indent = 2, encoding = "utf-8")) # reescreve em formato de json (e não ascii), indet é o tab
+    DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, indent = 2), encoding = "utf-8") # reescreve em formato de json (e não ascii), indet é o tab
+
+# FUNÇÃO QUE BUSCA LEADS DE ACORDO COM A QUERY E RETORNA UMA LISTA COM RESULTADOS
+def read_leads_search(query):
+    leads = read_leads() # lista de leads / lista de dicts / array of dicts
+    results = []
+
+    for i, lead in enumerate(leads):
+        txt_lead = f"{lead["name"]} {lead["email"]}".lower()
+        if query.lower() in txt_lead:
+            results.append((i, lead))
+    return results
+
+def export_csv():
+    """ Exporta os leads para csv e RETORNA o caminho ao arquivo csv"""
+    path_csv = DATA_DIR / "Leads.csv"
+    leads = read_leads()
+
+    try:
+        with path_csv.open ("w", newline = "", encoding = "utf-8") as file_csv:
+            writer = csv.DictWriter(file_csv, leads[0].keys())
+            writer.writeheader()
+            for row_dict in leads:
+                writer.writerow(row_dict)
+        return path_csv
+    except PermissionError:
+        return None
+
